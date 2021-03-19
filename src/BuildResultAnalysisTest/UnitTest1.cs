4e97fae2745280f3955165c218c69712d645878a
Chad Nedzlek <ChadNedzlek@users.noreply.github.com>
2021-03-19 15:47:31 -0700

    DO NOT MERGE - Chad Test comment

--- src/BuildResultAnalysisTest/UnitTest1.cs
+++ src/BuildResultAnalysisTest/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace BuildResultAnalysisTest
         [Test]
         public void TestAlwaysFailing()
         {
+            // Chad's weird comment
             Assert.Fail();
         }
     }
