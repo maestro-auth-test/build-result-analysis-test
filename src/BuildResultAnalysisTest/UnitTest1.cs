3d3a22d3c08193bb4ed042b6df37b819a0031c88
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-04-07 12:38:47 -0700

    more tests

--- src/BuildResultAnalysisTest/UnitTest1.cs
+++ src/BuildResultAnalysisTest/UnitTest1.cs
@@ -11,7 +11,25 @@ namespace BuildResultAnalysisTest
         }
 
         [Test]
-        public void TestAlwaysFailingSecond()
+        public void TestAlwaysFailing3()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void TestAlwaysFailing4()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void TestAlwaysFailing5()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void TestAlwaysFailing6()
         {
             Assert.Fail();
         }
