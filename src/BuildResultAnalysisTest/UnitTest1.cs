fc54ee7511e1a553f75ea74d21b308a96fb610ba
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-04-08 13:51:22 -0700

    adding stage

--- src/BuildResultAnalysisTest/UnitTest1.cs
+++ src/BuildResultAnalysisTest/UnitTest1.cs
@@ -19,5 +19,35 @@ namespace BuildResultAnalysisTest
                 Assert.Pass();
             }
         }
+
+        [Test]
+        public void TestFailingRandomlyS2()
+        {
+            Random rand = new Random();
+            bool result = rand.Next(0, 2) != 0;
+            if (result)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void TestFailingRandomlyT3()
+        {
+            Random rand = new Random();
+            bool result = rand.Next(0, 2) != 0;
+            if (result)
+            {
+                Assert.Fail();
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
     }
 }
