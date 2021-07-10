30590361d87ca5754ff646ed95ad8daead056819
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-07-09 17:29:23 -0700

    fail once then pass

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace TestRetriesTests
@@ -16,30 +18,6 @@ namespace TestRetriesTests
             }
 
             Assert.True(exists);
-        }
-
-        [Fact]
-        public void FailThreeTimesThenPass()
-        {
-            var target = Path.Combine(Path.GetTempPath(), "my-test-file-123456.snt");
-            bool exists = File.Exists(target);
-            var all = "";
-            if (exists)
-            {
-                all = File.ReadAllText(target);
-                File.WriteAllText(target, "Test failed twice");
-                if (all.Contains("Test failed twice"))
-                {
-                    File.WriteAllText(target, "Test failed three times");
-                }
-
-            }
-            if (!exists)
-            {
-                File.WriteAllText(target, "Test failed once");
-            }
-
-            Assert.Contains("Test failed three times", all);
         }
     }
 }
