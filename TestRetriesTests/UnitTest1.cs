c4fce9f6cd5e290a67cf2b19a944be29283f986a
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-07-12 13:48:09 -0700

    subresults + data driven

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -7,10 +7,12 @@ namespace TestRetriesTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void FailOnceThenPass()
+        [Theory]
+        [InlineData("my-test-file-123456.snt")]
+        [InlineData("my-test-file-123457.snt")]
+        public void FailOnceThenPass(string fileName)
         {
-            var target = Path.Combine(Path.GetTempPath(), "my-test-file-123456.snt");
+            var target = Path.Combine(Path.GetTempPath(), fileName);
             bool exists = File.Exists(target);
             if (!exists)
             {
@@ -18,30 +20,6 @@ namespace TestRetriesTests
             }
 
             Assert.True(exists);
-        }
-
-        [Fact]
-        public void FailThreeTimesThenPass()
-        {
-            var target = Path.Combine(Path.GetTempPath(), "my-test-file-123457.snt");
-            bool exists = File.Exists(target);
-            var all = "";
-            if (exists)
-            {
-                all = File.ReadAllText(target);
-                File.WriteAllText(target, "Test failed twice");
-                if (all.Contains("Test failed twice"))
-                {
-                    File.WriteAllText(target, "Test failed three times");
-                }
-
-            }
-            if (!exists)
-            {
-                File.WriteAllText(target, "Test failed once");
-            }
-
-            Assert.Contains("Test failed three times", all);
         }
     }
 }
