3dca7ec1eb3ee282f22425b5316ca146675799b3
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-07-12 10:08:46 -0700

    multiple failures

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -8,16 +8,27 @@ namespace TestRetriesTests
     public class UnitTest1
     {
         [Fact]
-        public void FailOnceThenPass()
+        public void FailThreeTimesThenPass()
         {
             var target = Path.Combine(Path.GetTempPath(), "my-test-file-123456.snt");
             bool exists = File.Exists(target);
+            var all = "";
+            if (exists)
+            {
+                all = File.ReadAllText(target);
+                File.WriteAllText(target, "Test failed twice");
+                if (all.Contains("Test failed twice"))
+                {
+                    File.WriteAllText(target, "Test failed three times");
+                }
+
+            }
             if (!exists)
             {
                 File.WriteAllText(target, "Test failed once");
             }
 
-            Assert.True(exists);
+            Assert.Contains("Test failed three times", all);
         }
     }
 }
