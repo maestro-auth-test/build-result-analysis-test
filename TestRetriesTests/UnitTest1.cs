5937e4815f22f05eb6107602a9ff7b5a402aaf44
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-07-12 15:50:49 -0700

    trying to get a green build

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -7,10 +7,23 @@ namespace TestRetriesTests
 {
     public class UnitTest1
     {
+        [Fact]
+        public void FailOnceThenPass()
+        {
+            var target = Path.Combine(Path.GetTempPath(), "my-test-file-123454.snt");
+            bool exists = File.Exists(target);
+            if (!exists)
+            {
+                File.WriteAllText(target, "Test failed once");
+            }
+
+            Assert.True(exists);
+        }
+
         [Theory]
         [InlineData("my-test-file-123456.snt")]
         [InlineData("my-test-file-123457.snt")]
-        public void FailOnceThenPass(string fileName)
+        public void FailOnceThenPassDataDriven(string fileName)
         {
             var target = Path.Combine(Path.GetTempPath(), fileName);
             bool exists = File.Exists(target);
