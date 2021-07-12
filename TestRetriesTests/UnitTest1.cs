2a7b557ff94564e3007596a3abed9f77af70d87c
Alitzel Mendez Bustillo <almend@microsoft.com>
2021-07-12 10:47:12 -0700

    both

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -19,5 +19,29 @@ namespace TestRetriesTests
 
             Assert.True(exists);
         }
+
+        [Fact]
+        public void FailThreeTimesThenPass()
+        {
+            var target = Path.Combine(Path.GetTempPath(), "my-test-file-123457.snt");
+            bool exists = File.Exists(target);
+            var all = "";
+            if (exists)
+            {
+                all = File.ReadAllText(target);
+                File.WriteAllText(target, "Test failed twice");
+                if (all.Contains("Test failed twice"))
+                {
+                    File.WriteAllText(target, "Test failed three times");
+                }
+
+            }
+            if (!exists)
+            {
+                File.WriteAllText(target, "Test failed once");
+            }
+
+            Assert.Contains("Test failed three times", all);
+        }
     }
 }
