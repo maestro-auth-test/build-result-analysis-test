d5c0ccad822b1e34636fb6af6226c6d7740638b9
Alitzel Mendez <alichel_6288@hotmail.com>
2021-12-01 15:20:06 -0800

    Definit test result in text

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -1,23 +1,45 @@
 using System;
 using System.IO;
-using System.Linq;
 using Xunit;
 
-namespace TestRetriesTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void FailOnceThenPass()
-        {
-            Assert.True(true);
-            
-        }
+namespace TestRetriesTests;
 
-        [Fact]
-        public void AlwaysPassing()
-        {
-            Assert.True(true);
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Attempt1FailOnceThenPass()
+    {
+        var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "myresult123456","my-result-123456.txt");
+        var result = false;
+
+        var lines = File.ReadAllLines(target);
+
+        foreach (var line in lines)
+            if (line.Contains("True"))
+                result = true;
+
+        Assert.True(result);
+    }
+
+
+    [Fact]
+    public void FailOnceThenPass()
+    {
+        var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "my-result-123456.txt");
+        var result = false;
+
+        var lines = File.ReadAllLines(target);
+
+        foreach (var line in lines)
+            if (line.Contains("True"))
+                result = true;
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void AlwaysPassing()
+    {
+        Assert.True(true);
     }
 }
