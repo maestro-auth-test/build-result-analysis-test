0628041633658a6adf9a3c03efae6de7cbee5291
Alitzel Mendez <alichel_6288@hotmail.com>
2021-12-01 16:34:32 -0800

    Accidentally removed the namespace

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -2,44 +2,46 @@ using System;
 using System.IO;
 using Xunit;
 
-namespace TestRetriesTests;
-
-public class UnitTest1
+namespace TestRetriesTests
 {
-    [Fact]
-    public void Attempt1FailOnceThenPass()
+    public class UnitTest1
     {
-        var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "myresult123456","my-result-123456.txt");
-        var result = false;
+        [Fact]
+        public void Attempt1FailOnceThenPass()
+        {
+            var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "myresult123456", "my-result-123456.txt");
+            var result = false;
 
-        var lines = File.ReadAllLines(target);
+            var lines = File.ReadAllLines(target);
 
-        foreach (var line in lines)
-            if (line.Contains("True"))
-                result = true;
+            foreach (var line in lines)
+                if (line.Contains("True"))
+                    result = true;
 
-        Assert.True(result);
-    }
+            Assert.True(result);
+        }
 
 
-    [Fact]
-    public void FailOnceThenPass()
-    {
-        var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "my-result-123456.txt");
-        var result = false;
+        [Fact]
+        public void FailOnceThenPass()
+        {
+            var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"), "my-result-123456.txt");
+            var result = false;
 
-        var lines = File.ReadAllLines(target);
+            var lines = File.ReadAllLines(target);
 
-        foreach (var line in lines)
-            if (line.Contains("True"))
-                result = true;
+            foreach (var line in lines)
+                if (line.Contains("True"))
+                    result = true;
 
-        Assert.True(result);
-    }
+            Assert.True(result);
+        }
 
-    [Fact]
-    public void AlwaysPassing()
-    {
-        Assert.True(true);
+        [Fact]
+        public void AlwaysPassing()
+        {
+            Assert.True(true);
+        }
     }
 }
+
