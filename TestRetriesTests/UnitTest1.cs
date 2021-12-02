d30a02000a343e97b6c50b2d3779810f2a04e77e
Alitzel Mendez <alichel_6288@hotmail.com>
2021-12-02 09:42:51 -0800

    Passing test

--- TestRetriesTests/UnitTest1.cs
+++ TestRetriesTests/UnitTest1.cs
@@ -15,40 +15,6 @@ namespace TestRetriesTests
         }
 
         [Fact]
-        public void Attempt1FailOnceThenPass()
-        {
-            var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_WORKITEM_PAYLOAD"), "myresult123456", "my-result-123456.txt");
-            var result = false;
-
-            var lines = File.ReadAllLines(target);
-
-            foreach (var line in lines)
-                if (line.Contains("True"))
-                    result = true;
-
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void GetFiles()
-        {
-            string[] fileEntries = Directory.GetFiles(Environment.GetEnvironmentVariable("HELIX_CORRELATION_PAYLOAD"));
-            foreach (string fileName in fileEntries)
-            {
-                _testOutputHelper.WriteLine("Processed file HELIX_WORKITEM_PAYLOAD '{0}'.", fileName);
-            }
-
-
-            string[] fileEntriesSecond = Directory.GetFiles(Environment.GetEnvironmentVariable("HELIX_WORKITEM_ROOT"));
-            foreach (string fileName in fileEntriesSecond)
-            {
-                _testOutputHelper.WriteLine("Processed file HELIX_WORKITEM_ROOT '{0}'.", fileName);
-            }
-
-            Assert.False(true);
-        }
-
-        [Fact]
         public void FailOnceThenPass()
         {
             var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_CORRELATION_PAYLOAD"), "my-result-123456.txt");
