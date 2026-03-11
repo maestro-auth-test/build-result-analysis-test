32430c03f7cd346d5af752f19cfe0840f6f023fc
Premek Vysoky <premek.vysoky@microsoft.com>
2026-03-11 13:08:04 +0100

    Add known issue failure

--- src/TestAggregatorHelixTests/TestAggregatorHelixTests.cs
+++ src/TestAggregatorHelixTests/TestAggregatorHelixTests.cs
@@ -1,26 +1,31 @@
 using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 
-namespace TestAggregatorHelixTests
+namespace TestAggregatorHelixTests;
+
+public class TestAggregatorHelixTests
 {
-    public class TestAggregatorHelixTests
+    // This test is run several times
+    // Based on the conent in the correlation payload file, it behaves differently, simulating different test results (pass, fail, known issue)
+    [Fact]
+    public void TestResultDeterminedByCorrelationPayload()
     {
-        //This test is run twice, one with correlation payload message: 'Pass' and other with correlation payload message: 'Fail'
-        [Fact]
-        public void TestResultDeterminedByCorrelationPayload()
+        var target = Path.Combine(
+            Environment.GetEnvironmentVariable("HELIX_CORRELATION_PAYLOAD"),
+            "my-result-123456.txt");
+
+        var lines = File.ReadAllLines(target);
+
+        if (lines.Any(l => l.Contains("KnownIssue")))
         {
-            var result = false;
-            var target = Path.Combine(Environment.GetEnvironmentVariable("HELIX_CORRELATION_PAYLOAD"),
-                "my-result-123456.txt");
+            Assert.True(false, "This failure will be matched from a known issue");
+        }
 
-            var lines = File.ReadAllLines(target);
-
-            foreach (var line in lines)
-                if (line.Contains("Pass"))
-                    result = true;
-
-            Assert.True(result);
+        if (lines.Any(l => l.Contains("Fail")))
+        {
+            Assert.True(false);
         }
     }
 }
